92a121e38e0aa8adfb830c26117fd9d4838677aa
Keila-Camarillo <126615506+Keila-Camarillo@users.noreply.github.com>
2023-05-24 12:31:38 -0500

    update

--- keila_explore.py
+++ keila_explore.py
@@ -22,10 +22,42 @@ def ind_test(samp1, samp2, alpha=0.05):
         print(f''' Fail to reject the null: Insufficient evidence''')
     print(f" p-value: {p} , t: {t}")
 
+def ind_var_test(samp1, samp2, alpha=0.05):
+    '''
+    Completes an sample t-test, based on the null hypothesis less than
+    '''
+    t, p = stats.ttest_ind(samp1, samp2)
+
+    if p/2 < alpha and t > 0 :
+        print(f'''Reject the null hypothesis: Sufficient''')
+    else:
+        print(f''' Fail to reject the null: Insufficient evidence''')
+    print(f" p-value: {p} , t: {t}")
+
+def alcohol_barplot(df):
+    '''
+    This function creates a custom bar chart for comparing low/high quality wine's alcohol content
+    '''
+    fig, ax =plt.subplots()
+    
+    # set color palette
+    sns.set_palette("pastel")
+    
+    # create average line    
+    plt.title("High Quality Wine has More Alcohol")
+    sns.barplot(x="quality_type", y="alcohol", data=df, hue='wine_type')
+    plt.xlabel("Quality")
+    plt.ylabel("Alcohol Content")
+    tick_label = ["Low", "High"]
+    ax.set_xticklabels(tick_label)
+    property_value_average = df.alcohol.mean()
+    plt.axhline(property_value_average, label="Alcohol Content Average", color='DarkSlateBlue')
+    plt.legend(loc='lower right')
+    plt.show()
 
 def sugar_barplot(df):
     '''
-    This function creates a custom bar chart for comparing homes with pools and homes without pools
+    This function creates a custom bar chart for comparing the residual sugar in low/high quality wine.
     '''
     fig, ax =plt.subplots()
     
@@ -42,4 +74,46 @@ def sugar_barplot(df):
     property_value_average = df.residual_sugar.mean()
     plt.axhline(property_value_average, label="Residual Sugar Average", color='DarkSlateBlue')
     plt.legend(loc='upper right')
+    plt.show()
+
+def chlorides_barplot(df):
+    '''
+    This function creates a custom bar chart for comparing low/high quality wine's chlorides
+    '''
+    fig, ax =plt.subplots()
+    
+    # set color palette
+    sns.set_palette("pastel")
+    
+    # create average line    
+    plt.title("Low Quality Wine has More Chlorides")
+    sns.barplot(x="quality_type", y="chlorides", data=df, hue='wine_type')
+    plt.xlabel("Quality")
+    plt.ylabel("Chlorides")
+    tick_label = ["Low", "High"]
+    ax.set_xticklabels(tick_label)
+    property_value_average = df.chlorides.mean()
+    plt.axhline(property_value_average, label="Chlorides Average", color='DarkSlateBlue')
+    plt.legend(loc='lower right')
+    plt.show()
+
+def tsd_barplot(df):
+    '''
+    This function creates a custom bar chart for comparing low/high quality wine's total sulfur dioxide content
+    '''
+    fig, ax =plt.subplots()
+    
+    # set color palette
+    sns.set_palette("pastel")
+    
+    # create average line    
+    plt.title("Higher Quality Wine has Less Total Sulfur Dioxide")
+    sns.barplot(x="quality_type", y="total_sulfur_dioxide", data=df, hue='wine_type')
+    plt.xlabel("Quality")
+    plt.ylabel("Total Sulfur Dioxide")
+    tick_label = ["Low", "High"]
+    ax.set_xticklabels(tick_label)
+    total_sulfur_dioxide = df.total_sulfur_dioxide.mean()
+    plt.axhline(total_sulfur_dioxide, label="Total Sulfur Dioxide Average", color='DarkSlateBlue')
+    plt.legend(loc='lower right')
     plt.show()
